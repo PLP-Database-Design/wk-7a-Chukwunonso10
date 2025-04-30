7e821d87d96ad14bc0f0134a736f342865d02423
Chukwunonso10 <kuzuechinonsojude@gmail.com>
2025-04-30 07:35:48 +0100

    assignment solutions

---
+++ answers.sql
@@ -0,0 +1,28 @@
+-- QUESTION 1
+-- No partial dependencies allowed.
+
+-- Recreate the table in 1NF
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+
+
+-- QUESTION 2
+
+-- Separate table for customers
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+
+-- Table with only Product and Quantity
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
